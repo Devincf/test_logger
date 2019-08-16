72315922e5bad6bd86dc690dc2c93f13847da6db
Devin Firat <devinc.firat@gmail.com>
2019-08-16 12:58:34 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+def main():
+	print("{\"test3\":\"b85daa15-fe75-4f6d-b280-80b6f520073\"}")
+	print("{\"test4\":{\"bkey\":\"b85daa15-fe75-4f6d-b280-80b6f520073\"}}")
+
+
+if __name__ == "__main__":
+    main()
